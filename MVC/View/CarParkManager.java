83e6d3438ac3c85f7e220cbf324fa926cfd70477
AsmMahim65 <abushale.mahim@gmail.com>
2021-04-29 23:32:08 +0600

    Final

---
+++ MVC/View/CarParkManager.java
@@ -0,0 +1,17 @@
+package View;
+
+import Model.*;
+import Controller.*;
+
+public interface CarParkManager  {
+//interface of the system
+
+    void displayMainMenu();
+    void addVehicle();
+    void deleteVehicle();
+    void printVehicleList();
+    void calculatePercentages();
+    void displayVehicleStats();
+    void calculateParkingCharges();
+    void displayParkingLog();
+}
